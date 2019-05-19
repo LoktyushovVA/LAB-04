4ee51fe3774ad4c004090f3315128f701ac4686a
LoktyushoVA <vladimir@mail.ru>
2019-05-19 21:12:39 +0300

    code:преобразовали void* uint_t* и напечатали массив этих байтов

--- main.cpp
+++ main.cpp
@@ -8,3 +8,23 @@ void print_in_hex (uint8_t byte)
     cout << nibble_to_hex(byte >> 4)
          << nibble_to_hex(byte & 0xf);
 }
+const uint8_t* as_bytes(const void* data)
+{
+    return reinterpret_cast<const uint8_t*>(data);
+}
+void print_in_hex(const void* data, size_t size)
+{
+    const uint8_t* bytes = as_bytes(data);
+    for (size_t i = 0; i < size; i++)
+    {
+        print_in_hex(bytes[i]);
+        if ((i + 1) % 16 == 0)
+        {
+            cout << '\n';
+        }
+        else
+        {
+            cout << ' ';
+        }
+    }
+}
